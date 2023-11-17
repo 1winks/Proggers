1e8b4d5b074d0be16224b9d5ab6fd17792ae703d
lk534 <lk53734@fer.hr>
2023-11-17 01:15:09 +0100

    MVC in SecurityFilterChain

--- IzvorniKod/backend/src/main/java/org/proggers/backend/rest/WebSecurityBasic.java
+++ IzvorniKod/backend/src/main/java/org/proggers/backend/rest/WebSecurityBasic.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 // TODO: Figure out what is actually needed in this class and what isnt
 
@@ -16,17 +18,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = false)
 public class WebSecurityBasic {
     @Bean
-    @Profile("basic-security")
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                    .requestMatchers("/").permitAll()
-                    .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
+                    .requestMatchers(mvc.pattern("/"), mvc.pattern("/home")).permitAll()
+                    .requestMatchers(mvc.pattern("/admin/*")).hasAuthority("ROLE_ADMIN")
                     .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
                 .logout((logout) -> logout.permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(AbstractHttpConfigurer::disable); // TODO: CSRF can cause issues with redirect to /login page, investigate.
 
         return http.build();
     }
