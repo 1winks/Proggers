877cca284c9156a64cb9ec0a9a2af9a153754914
Božidar Pučar <bp003523336@fer.hr>
2024-01-19 22:10:01 +0100

    Uređen BookBox.jsx file. Dodan prikaz slike korica kad nema slike u bazi podataka

--- IzvorniKod/frontend/Što želiš čitati/src/components/BookList/BookBox.jsx
+++ IzvorniKod/frontend/Što želiš čitati/src/components/BookList/BookBox.jsx
@@ -1,6 +1,7 @@
 import react, { useState } from "react";
-import image from "../../images/bookCover.jpg";
+import defaultImage from "../../images/bookCover.jpg";
 import BookDetails from "../BookDetails/BookDetails";
+
 
 const BookBox = ({ book }) => {
     const [show, setShow]=useState(false);
@@ -11,18 +12,30 @@ const BookBox = ({ book }) => {
             {
                 book.map((item) => {
                     let image = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-                    if(image != undefined)
-                    {
+                    if (image != undefined) {
                         return (
                             <>
                                 <div className="bookCard" onClick={() => { setShow(true); setItem(item) }}>
-                                <img src={image} alt="" />
-                                <div className="bookCardBottom">
+                                    <img src={image} alt="" />
+                                    <div className="bookCardBottom">
                                         <h3 className="bookCardTitle">{item.volumeInfo.title}</h3>
                                         <h3 className="bookCardAuthor">{item.volumeInfo.autors}</h3>
+                                    </div>
                                 </div>
-                            </div>
-                                <BookDetails show={show} item={bookItem} onClose={()=>setShow(false)}/>    
+                                <BookDetails show={show} item={bookItem} onClose={() => setShow(false)} />
+                            </>
+                        )
+                    } else {
+                        return (
+                            <>
+                                <div className="bookCard" onClick={() => { setShow(true); setItem(item) }}>
+                                    <img src={defaultImage} alt="" />
+                                    <div className="bookCardBottom">
+                                        <h3 className="bookCardTitle">{item.volumeInfo.title}</h3>
+                                        <h3 className="bookCardAuthor">{item.volumeInfo.autors}</h3>
+                                    </div>
+                                </div>
+                                <BookDetails show={show} item={bookItem} onClose={() => setShow(false)} />
                             </>
                         )
                     }
